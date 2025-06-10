9b28058e2f4ca0db637ae73c86561f6de38f1c7a
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-06-10 19:53:20 +0530

    Create App.js

---
+++ src/frontend/App.js
@@ -0,0 +1,16 @@
+```javascript
+import React from 'react';
+import PatientList from './components/PatientList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Patient Management System</h1>
+      <PatientList />
+    </div>
+  );
+}
+
+export default App;
+```
